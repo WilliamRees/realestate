1930524a22e978cd4bfec612072d2d6ad603a37b
kaue <kaue@email.com>
2015-04-20 00:15:36 -0400

    Added new missing pages, updated CSS and Views

--- featured_listings.php
+++ featured_listings.php
@@ -17,7 +17,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>#TODO</title>
+        <title>Mustafa Zia | Featured Listings</title>
         <meta name="description" content="#TODO">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -56,7 +56,7 @@
 
         <?php if (isset($_GET["status"]) && $_GET["status"] == "removed"): ?>
             <div id="StatusMessage">
-                The listing you're looking for has been removed.
+                The listing you looking for is no longer available, please check our current listings below:
             </div>
         <?php endif; ?>  
 
